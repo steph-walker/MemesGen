2248bb74ad41916c95e7e0eb5a000c6140c19852
Stephanie <stephanie.walker@sada.com>
2021-03-31 15:36:19 -0700

    deleted all html pages

--- Memes.php
+++ Memes.php
@@ -6,13 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Memes</title>
     <!-- Favicon -->
-    <link rel="apple-touch-icon" sizes="180x180" href="./assets/css/images/favicon/apple-touch-icon.png">
+    <!-- <link rel="apple-touch-icon" sizes="180x180" href="./assets/css/images/favicon/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="./assets/css/images/favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="./assets/css/images/favicon/favicon-16x16.png">
     <link rel="manifest" href="./assets/css/images/favicon/site.webmanifest">
     <link rel="mask-icon" href="./assets/css/images/favicon/safari-pinned-tab.svg" color="#5bbad5">
     <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="theme-color" content="#ffffff">
+    <meta name="theme-color" content="#ffffff"> -->
+
     <!-- Main font -->
     <link href="./assets/css/lato-font.css" rel="stylesheet">
     <!-- Fontawesome -->
@@ -45,7 +46,6 @@
                 <ul>
                     
                     <li><a href="index.php">Top Memes</a></li>
-                    <li ><a href="profile.html">Profile</a></li>
                     <li class="current"> <a href="Memes.php">Meme Templates</a></li>
                     <li>
                     </li>
--- index.php
+++ index.php
@@ -25,107 +25,12 @@
 									<li class="current"><a href="index.php">Top Memes</a></li>
 									<li><a href="Memes.php">Meme Templates</a></li>																	
 								</ul>
-								<div class="cd-main-nav js-main-nav">
-									<ul class="cd-main-nav__list js-signin-modal-trigger">
-									   <!-- inser more links here -->
-									   <li><a class="cd-main-nav__item cd-main-nav__item--signin" href="#0" data-signin="login">Sign in</a></li>
-									   <li><a href="profile.html">Profile</a></li>	
-									</ul>
-								</div>
+							
 								
 					</header>
 				</div>
 
-			<!-- Banner -->
-			
-			<div class="cd-signin-modal js-signin-modal"> <!-- this is the entire modal form, including the background -->
-				<div class="cd-signin-modal__container"> <!-- this is the container wrapper -->
-					<ul class="cd-signin-modal__switcher js-signin-modal-switcher js-signin-modal-trigger" style="list-style-type: none;">
-						<li><a href="#0" data-signin="login" data-type="login">Sign in</a></li>
-						<li><a href="#0" data-signin="signup" data-type="signup">New account</a></li>
-					</ul>
 		
-					<div class="cd-signin-modal__block js-signin-modal-block" data-type="login"> <!-- log in form -->
-						<form class="cd-signin-modal__form">
-							<p class="cd-signin-modal__fieldset">
-								<label class="cd-signin-modal__label cd-signin-modal__label--email cd-signin-modal__label--image-replace" for="signin-email">E-mail</label>
-								<input class="cd-signin-modal__input cd-signin-modal__input--full-width cd-signin-modal__input--has-padding cd-signin-modal__input--has-border" id="signin-email" type="email" placeholder="E-mail">
-								<span class="cd-signin-modal__error">Error message here!</span>
-							</p>
-		
-							<p class="cd-signin-modal__fieldset">
-								<label class="cd-signin-modal__label cd-signin-modal__label--password cd-signin-modal__label--image-replace" for="signin-password">Password</label>
-								<input class="cd-signin-modal__input cd-signin-modal__input--full-width cd-signin-modal__input--has-padding cd-signin-modal__input--has-border" id="signin-password" type="text"  placeholder="Password">
-								<a href="#0" class="cd-signin-modal__hide-password js-hide-password">Hide</a>
-								<span class="cd-signin-modal__error">Error message here!</span>
-							</p>
-		
-							<p class="cd-signin-modal__fieldset">
-								<input type="checkbox" id="remember-me" checked class="cd-signin-modal__input ">
-								<label for="remember-me">Remember me</label>
-							</p>
-		
-							<p class="cd-signin-modal__fieldset">
-								<input class="cd-signin-modal__input cd-signin-modal__input--full-width" type="submit" value="Login">
-							</p>
-						</form>
-						
-						<p class="cd-signin-modal__bottom-message js-signin-modal-trigger"><a href="#0" data-signin="reset">Forgot your password?</a></p>
-					</div> <!-- cd-signin-modal__block -->
-		
-					<div class="cd-signin-modal__block js-signin-modal-block" data-type="signup"> <!-- sign up form -->
-						<form class="cd-signin-modal__form">
-							<p class="cd-signin-modal__fieldset">
-								<label class="cd-signin-modal__label cd-signin-modal__label--username cd-signin-modal__label--image-replace" for="signup-username">Username</label>
-								<input class="cd-signin-modal__input cd-signin-modal__input--full-width cd-signin-modal__input--has-padding cd-signin-modal__input--has-border" id="signup-username" type="text" placeholder="Username">
-								<span class="cd-signin-modal__error">Error message here!</span>
-							</p>
-		
-							<p class="cd-signin-modal__fieldset">
-								<label class="cd-signin-modal__label cd-signin-modal__label--email cd-signin-modal__label--image-replace" for="signup-email">E-mail</label>
-								<input class="cd-signin-modal__input cd-signin-modal__input--full-width cd-signin-modal__input--has-padding cd-signin-modal__input--has-border" id="signup-email" type="email" placeholder="E-mail">
-								<span class="cd-signin-modal__error">Error message here!</span>
-							</p>
-		
-							<p class="cd-signin-modal__fieldset">
-								<label class="cd-signin-modal__label cd-signin-modal__label--password cd-signin-modal__label--image-replace" for="signup-password">Password</label>
-								<input class="cd-signin-modal__input cd-signin-modal__input--full-width cd-signin-modal__input--has-padding cd-signin-modal__input--has-border" id="signup-password" type="text"  placeholder="Password">
-								<a href="#0" class="cd-signin-modal__hide-password js-hide-password">Hide</a>
-								<span class="cd-signin-modal__error">Error message here!</span>
-							</p>
-		
-							<p class="cd-signin-modal__fieldset">
-								<input type="checkbox" id="accept-terms" class="cd-signin-modal__input ">
-								<label for="accept-terms">I agree to the <a href="#0">Terms</a></label>
-							</p>
-		
-							<p class="cd-signin-modal__fieldset">
-								<input class="cd-signin-modal__input cd-signin-modal__input--full-width cd-signin-modal__input--has-padding" type="submit" value="Create account">
-							</p>
-						</form>
-					</div> <!-- cd-signin-modal__block -->
-		
-					<div class="cd-signin-modal__block js-signin-modal-block" data-type="reset"> <!-- reset password form -->
-						<p class="cd-signin-modal__message">Lost your password? Please enter your email address. You will receive a link to create a new password.</p>
-		
-						<form class="cd-signin-modal__form">
-							<p class="cd-signin-modal__fieldset">
-								<label class="cd-signin-modal__label cd-signin-modal__label--email cd-signin-modal__label--image-replace" for="reset-email">E-mail</label>
-								<input class="cd-signin-modal__input cd-signin-modal__input--full-width cd-signin-modal__input--has-padding cd-signin-modal__input--has-border" id="reset-email" type="email" placeholder="E-mail">
-								<span class="cd-signin-modal__error">Error message here!</span>
-							</p>
-		
-							<p class="cd-signin-modal__fieldset">
-								<input class="cd-signin-modal__input cd-signin-modal__input--full-width cd-signin-modal__input--has-padding" type="submit" value="Reset password">
-							</p>
-						</form>
-		
-						<p class="cd-signin-modal__bottom-message js-signin-modal-trigger"><a href="#0" data-signin="login">Back to log-in</a></p>
-					</div> <!-- cd-signin-modal__block -->
-					<a href="#0" class="cd-signin-modal__close js-close">Close</a>
-				</div> <!-- cd-signin-modal__container -->
-			</div> <!-- cd-signin-modal -->
-
 
 
 				
@@ -163,17 +68,15 @@
 
 								<!-- Contact -->
 									<section class="widget contact last">
-										<h3>Contact Us</h3>
+									
 										<ul>
-											<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
-											<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
-											<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
-											<li><a href="#" class="icon brands fa-dribbble"><span class="label">Dribbble</span></a></li>
-											<li><a href="#" class="icon brands fa-pinterest"><span class="label">Pinterest</span></a></li>
+											<li><a href="https://twitter.com/SADA" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+											<li><a href="https://www.facebook.com/SADASystems" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+											<li><a href="https://www.youtube.com/channel/UC57cHEmW6dSt5IP1aJI9FUg" class="icon brands fa-youtube"><span class="label">Instagram</span></a></li>
+											<li><a href="https://www.linkedin.com/company/sada/" class="icon brands fa-linkedin"><span class="label">LinkedIn</span></a></li>
+											
 										</ul>
-										<p>1234 Fictional Road<br />
-										Nashville, TN 00000<br />
-										(800) 555-0000</p>
+										
 									</section>
 
 							</div>
@@ -182,7 +85,7 @@
 							<div class="col-12">
 								<div id="copyright">
 									<ul class="menu">
-										<li>&copy; Untitled. All rights reserved</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+										<li>&copy; 2021 SADA, Inc. All Rights Reserved</li><li><a href="https://sada.com/privacy-policy/">Privacy Policy </a></li> <li> <a href="https://sada.com/terms-of-use/">Terms Of Use</a></li>
 									</ul>
 								</div>
 							</div>
